c4580fb0ee1ec679d777886f89d9731a7232c02e
MathieuCoding <polletmathieu.pro@gmail.com>
2023-02-21 10:50:04 +0100

    cards added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,19 +5,27 @@ export function Search()
 
     const [query, setQuery] = useState("");
     const [result, setResult] = useState([]);
-
+    
     useEffect(() => {
         fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
         .then((response) => response.json())
         .then((data) => setResult(data));
     });
 
-    const show = result.map((show) =>  <li key={show.show.id}>{show.show.name}</li>);
+    console.log(result);
+    const show = result.map((show) => 
+    <div className="card-show">
+        <p key={show.show.id}>{show.show.name}</p>
+        {show.show.image != null && <img src={show.show.image.medium} alt="sunshine in a tree" width="200px" height="auto"/> }       
+    </div>
+    );
 
     return (
         <div>
             <input type="text" onChange={(e) => setQuery(e.target.value)} value={query}/>
-            <ul>{show}</ul>
+            <div className="card-container">
+                {show}
+            </div>
         </div>
     )      
 }
