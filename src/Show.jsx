26e8cd0e7b1b39d9fb4350da4e53b9ea16ce4e78
MathieuCoding <polletmathieu.pro@gmail.com>
2023-02-22 16:14:57 +0100

    conditions on parameters

--- src/Show.jsx
+++ src/Show.jsx
@@ -19,14 +19,18 @@ export function Show()
         return(
             
             <div className="show-container">
-                {result.image != null ? <img src={result.image.medium} alt={result.name} width="30%" height="auto"/> : <img src="https://images.pexels.com/photos/3962259/pexels-photo-3962259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="we are closed sign" width="200px" height="auto"/>}
+                {result.image != null ? <img src={result.image.medium} alt={result.name} width="30%" height="auto"/> : <img src="https://images.pexels.com/photos/3962259/pexels-photo-3962259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="We are closed sign" width="200px" height="auto"/>}
                 <h1>{result.name}</h1>
                 <h4>Release date: {result.premiered}</h4>
-                <h4>Genres:</h4> {result.genres.map((genre) => genre + " ")}
-                <h4>Rating: {result.rating?.average}/10</h4>
+
+                {result.genres.length > 0 && <h4>Genres:</h4>}
+                {result.genres.map((genre) => <em key={genre}>{genre} </em>)}
+
+                {result.rating.average != null && <h4>Rating: {result.rating?.average}/10</h4>}
+
                 <h4>{parse(result.summary)}</h4>
-                <h4>Cast:</h4>
-                {result._embedded.cast.map((actor) => <p><Link to={'/actor/' + actor.person.id}>{actor.person.name}</Link></p>)}
+                <h4>Starring:</h4>
+                {result._embedded.cast.map((actor) => <p key={actor.person.id}><Link to={'/actor/' + actor.person.id}>{actor.person.name}</Link></p>)}
                 <Link to={'/'}><i className="fa-solid fa-house"></i></Link>
             </div>
         )
